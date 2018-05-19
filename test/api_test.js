dc5966cbe6855396aa51a2c9eb41fee958e19b96
Andrew Dymond <andy.dymond@gmail.com>
2018-05-19 16:53:23 -0600

    Add test returns error with invalid pair

--- test/api_test.js
+++ test/api_test.js
@@ -9,7 +9,7 @@ describe('API', () => {
   describe('GET pair rate', () => {
     it('returns pair title & current pair rate', () => {
       return chai.request(process.env.ROOT)
-      .get("/rate/btc_ltc")
+      .get('/rate/btc_ltc')
       .then((response) => {
         response.should.have.status(200)
         response.body.should.be.an('object')
@@ -21,6 +21,21 @@ describe('API', () => {
         let rate = response.body.rate
         rate.should.be.a('string')
         parseInt(rate).should.be.a('number')
+      })
+      .catch((error) => {
+        throw error
+      })
+    })
+
+    it('returns error with invalid pair', () => {
+      return chai.request(process.env.ROOT)
+      .get('/rate/btc_lt')
+      .then((response) => {
+        response.should.have.status(200)
+        response.body.should.be.an('object')
+
+        let error = response.body.error
+        error.should.eq('Pair btc_lt is currently unavailable.')
       })
       .catch((error) => {
         throw error
