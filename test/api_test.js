ab4d59ad0251d8854e837b4e6283db7c4dff306d
Andrew Dymond <andy.dymond@gmail.com>
2018-05-19 16:50:14 -0600

    Refactor GET pair rate to better legibility

--- test/api_test.js
+++ test/api_test.js
@@ -5,8 +5,8 @@ const figaroJSONPath = './node_modules/figaro/figaro.json'
 require('figaro').parse(figaroJSONPath, () => {})
 chai.use(chaiHTTP)
 
-describe("API", () => {
-  describe("GET pair rate", () => {
+describe('API', () => {
+  describe('GET pair rate', () => {
     it('returns pair title & current pair rate', () => {
       return chai.request(process.env.ROOT)
       .get("/rate/btc_ltc")
@@ -14,10 +14,13 @@ describe("API", () => {
         response.should.have.status(200)
         response.body.should.be.an('object')
 
-        response.body.pair.should.be.a('string')
-        isNaN(parseInt(response.body.pair)).should.eq(true)
-        response.body.rate.should.be.a('string')
-        parseInt(response.body.rate).should.be.a('number')
+        let pair = response.body.pair
+        pair.should.be.a('string')
+        isNaN(parseInt(pair)).should.eq(true)
+
+        let rate = response.body.rate
+        rate.should.be.a('string')
+        parseInt(rate).should.be.a('number')
       })
       .catch((error) => {
         throw error
