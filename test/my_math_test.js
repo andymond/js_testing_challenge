5f0898ec0e861d2636aee92b50c16aaeb943d621
Andrew Dymond <andy.dymond@gmail.com>
2018-05-19 16:13:55 -0600

    Add test for divide to my_math_test

--- test/my_math_test.js
+++ test/my_math_test.js
@@ -5,22 +5,29 @@ const MyMath = require('../lib/my_math.js');
 describe('MyMath', () => {
   describe('add', () => {
     it('adds two numbers together', () => {
-      const sum = MyMath.add(14, 15);
+      const sum = MyMath.add(14, 15)
       sum.should.eq(29);
     })
   })
 
   describe('subtract', () => {
     it('subtracts two numbers', () => {
-      const difference = MyMath.subtract(20, 14);
-      difference.should.eq(6);
+      const difference = MyMath.subtract(20, 14)
+      difference.should.eq(6)
     })
   })
 
   describe('multiply', () => {
     it('multiplies two numbers', () => {
-      const product = MyMath.multiply(3, 11);
-      product.should.eq(33);
+      const product = MyMath.multiply(3, 11)
+      product.should.eq(33)
+    })
+  })
+
+  describe('divide', () => {
+    it('divides two numbers', () => {
+      const quotient = MyMath.divide(3, 4)
+      quotient.should.eq(0.75)
     })
   })
 })
