55e8030e4be2493224abce76461294074ba88906
Andrew Dymond <andy.dymond@gmail.com>
2018-05-19 16:24:35 -0600

    Put function calls in functions to catch errors properly

--- test/my_math_test.js
+++ test/my_math_test.js
@@ -35,16 +35,16 @@ describe('MyMath', () => {
 describe('MyMath edge cases', () => {
   describe('non-numeric input', () => {
     it('rejects non-numeric input', () => {
-      const add = MyMath.add(1, 'a')
+      const add = () => {MyMath.add(1, 'a')}
       add.should.throw('Param is not a number!')
 
-      const subtract = MyMath.subtract(1, 'a')
+      const subtract = () => {MyMath.subtract('a', 1)}
       subtract.should.throw('Param is not a number!')
 
-      const multiply = MyMath.multiply(1, 'a')
+      const multiply = () => {MyMath.multiply({}, 'a')}
       multiply.should.throw('Param is not a number!')
-      
-      const divide = MyMath.divide(1, 'a')
+
+      const divide = () => {MyMath.divide('!', 'a')}
       divide.should.throw('Param is not a number!')
     })
   })
