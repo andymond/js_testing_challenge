b830f0abf17478ed8352be480f7685143f4fb2a5
Andrew Dymond <andy.dymond@gmail.com>
2018-05-19 16:12:01 -0600

    Add test for multiply to my_math_test

--- test/my_math_test.js
+++ test/my_math_test.js
@@ -7,11 +7,20 @@ describe('MyMath', () => {
     it('adds two numbers together', () => {
       const sum = MyMath.add(14, 15);
       sum.should.eq(29);
-    });
+    })
+  })
 
+  describe('subtract', () => {
     it('subtracts two numbers', () => {
       const difference = MyMath.subtract(20, 14);
       difference.should.eq(6);
     })
-  });
-});
+  })
+
+  describe('multiply', () => {
+    it('multiplies two numbers', () => {
+      const product = MyMath.multiply(3, 11);
+      product.should.eq(33);
+    })
+  })
+})
