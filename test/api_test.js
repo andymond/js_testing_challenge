5268a2816d3e969b4116253d3af644c94b74fb93
Andrew Dymond <andy.dymond@gmail.com>
2018-05-19 16:48:37 -0600

    Add api_test with GET pair rate test

--- test/api_test.js
+++ test/api_test.js
@@ -4,3 +4,24 @@ const chaiHTTP = require('chai-http')
 const figaroJSONPath = './node_modules/figaro/figaro.json'
 require('figaro').parse(figaroJSONPath, () => {})
 chai.use(chaiHTTP)
+
+describe("API", () => {
+  describe("GET pair rate", () => {
+    it('returns pair title & current pair rate', () => {
+      return chai.request(process.env.ROOT)
+      .get("/rate/btc_ltc")
+      .then((response) => {
+        response.should.have.status(200)
+        response.body.should.be.an('object')
+
+        response.body.pair.should.be.a('string')
+        isNaN(parseInt(response.body.pair)).should.eq(true)
+        response.body.rate.should.be.a('string')
+        parseInt(response.body.rate).should.be.a('number')
+      })
+      .catch((error) => {
+        throw error
+      })
+    })
+  })
+})
