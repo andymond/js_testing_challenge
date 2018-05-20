35e3509a5ad22582a043d76a99e4f6f1519138db
Andrew Dymond <andy.dymond@gmail.com>
2018-05-19 19:57:13 -0600

    Reorganize await functions & add getLocation to assert location after link clicked

--- test/ui_test.js
+++ test/ui_test.js
@@ -1,18 +1,29 @@
 import figaro from '../node_modules/figaro/figaro.json'
-import { Selector } from 'testcafe'
+import { Selector, ClientFunction } from 'testcafe'
 
 fixture `/#/coins`
   .page `${figaro.ROOT}/#/coins`
 
 test('navbar exists test', async t => {
   const nav = Selector('.nav')
-  await t
-    .expect(nav.exists).ok()
+
+  await t.expect(nav.exists).ok()
 })
 
 test('about link test', async t => {
   const about = Selector('.nav > li > a').nth(0)
+  const getLocation = ClientFunction(() => document.location.href);
+
+  await t.click(about)
+  await t.expect(getLocation()).contains('/about')
+})
+
+test('api link test', async t => {
+  const apiAndTools = Selector('.nav > li > a').nth(1)
+  const getLocation = ClientFunction(() => document.location.href);
 
   await t
-    .click(about)
+    .click(apiAndTools)
+    .click(Selector('.dropdown-menu > li > a').nth(0))
+  await t.expect(getLocation()).contains('/api')
 })
